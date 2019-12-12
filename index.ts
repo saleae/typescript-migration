7197935e132ed9b575a9c13e01af9d8be8f95963
Jacob Sansbury <sansburyjacob@gmail.com>
2019-12-12 11:24:36 -0800

    Add comments

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
 import { DeepCurrent } from "./model";
 
-interface Analyzer {
-  id: number;
-  type: "serial" | "spi" | "can";
-}
-
-interface InitialSaveConfig {
+// Define initial type and sub types
+interface V1 {
   version: {
     major: number;
     minor: number;
@@ -13,7 +9,13 @@ interface InitialSaveConfig {
   analzyers: Analyzer[];
 }
 
-const initialConfig: InitialSaveConfig = {
+interface Analyzer {
+  id: number;
+  type: "serial" | "spi" | "can";
+}
+
+// Setup an example instance of the initialConfig
+const initialConfig: V1 = {
   version: { major: 1, minor: 1 },
   analzyers: [
     {
@@ -23,6 +25,7 @@ const initialConfig: InitialSaveConfig = {
   ]
 };
 
+// Define the changes as the delth between V1 and V2
 interface DeltaV2 {
   version: string;
   numChannels: {
@@ -31,9 +34,15 @@ interface DeltaV2 {
   };
 }
 
-type V2 = DeepCurrent<InitialSaveConfig, DeltaV2>;
+// Deep merge the types to get V2
+type V2 = DeepCurrent<V1, DeltaV2>;
 
-const initialToV2 = (config: InitialSaveConfig): V2 => {
+/**
+ * Transform a V1 config into a V2 config
+ * @param config {V1} config
+ * @returns {V2} a new V2 config
+ */
+const initialToV2 = (config: V1): V2 => {
   return {
     ...config,
     version: [config.version.major, config.version.minor].join("."),
@@ -44,4 +53,5 @@ const initialToV2 = (config: InitialSaveConfig): V2 => {
   };
 };
 
+// display the result
 console.log(initialToV2(initialConfig));
